4cf70634053af603b0d5809190fd158a78dc76c9
Wastir <wastiros@gmail.com>
2024-03-27 16:23:14 +0100

    Added login, register and logout options

--- projekt/src/projekt/projekt/settings.py
+++ projekt/src/projekt/projekt/settings.py
@@ -31,9 +31,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
 # Application definition
-
+LOGIN_REDIRECT_URL = '../'
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,8 +47,25 @@ INSTALLED_APPS = [
 
     'produkty',
     'strony',
-]
 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+]
+SITE_ID = 1
+SOCIALACCOUNT_PROVIDERS = {
+    
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'projekt.urls'
--- projekt/src/projekt/projekt/urls.py
+++ projekt/src/projekt/projekt/urls.py
@@ -15,18 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from strony.views import home_view, about_view
 from produkty.views import products_view, product_detail
 from django.conf.urls.static import static
 from django.conf import settings
+from allauth.account.views import LoginView, ConfirmEmailView, LogoutView
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', products_view, name='home'),
     path('about/', about_view, name='about'),
     path('admin/', admin.site.urls),
     path('<int:id>', product_detail, name='product_detail'),
+    path('', include('allauth.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/', include('allauth.urls')),
+    path('account_confirm_email/', auth_views.LoginView.as_view(), name='account_email_verification_sent'),
+    path('account_confirm_email/<str:key>/', auth_views.LoginView.as_view(), name='account_confirm_email'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
